af60b88b593beac656686205283aad8a3faf3666
dogefromage <seb.sa@eblcom.ch>
2021-01-11 20:02:50 +0100

    copy previous code

---
+++ Unity Black Hole/Assets/CameraDrag.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+ 
+public class CameraDrag : MonoBehaviour
+{
+
+    public float dragSpeed = 2f;
+    public float scrollSensitivity = 1f;
+    private float distance = 5;
+
+    private Vector3 dragOrigin;
+    private float lastPhi = 0;
+    private float lastTheta = 0;
+    
+    public Transform camera;
+ 
+    void Update()
+    {
+        camera.localPosition = new Vector3(0, 0, -distance);
+        camera.localRotation = Quaternion.identity;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            dragOrigin = Input.mousePosition;
+            return;
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            lastPhi = camera.rotation.eulerAngles.y;
+            lastTheta = camera.rotation.eulerAngles.x;
+        }
+ 
+        if (!Input.GetMouseButton(0))
+        {
+            return;
+        }
+
+        distance *= Mathf.Exp(-Input.mouseScrollDelta.y * scrollSensitivity);
+        Debug.Log(Input.mouseScrollDelta.y);
+ 
+        Vector3 pos = Camera.main.ScreenToViewportPoint(Input.mousePosition - dragOrigin);
+        Vector3 move = new Vector3(pos.x * dragSpeed, 0, pos.y * dragSpeed);
+
+        float theta = lastTheta - pos.y * dragSpeed;
+        // theta = Mathf.Clamp(theta, -180, 180);
+        float phi = lastPhi + pos.x * dragSpeed;
+ 
+        transform.rotation = Quaternion.Euler(theta, phi, 0);
+    }
+}
---
+++ Unity Black Hole/Assets/RaymarchingMaster.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode()]
+public class RaymarchingMaster : MonoBehaviour
+{
+    // SHADER
+    public ComputeShader RaymarchingShader;
+    private RenderTexture _target;
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        SetShaderParameters();
+        Render(destination);
+    }
+    private void Render(RenderTexture destination)
+    {
+        // Make sure we have a current render target
+        InitRenderTexture();
+        // Set the target and dispatch the compute shader
+        RaymarchingShader.SetTexture(0, "Result", _target);
+        int threadGroupsX = Mathf.CeilToInt(Screen.width / 8.0f);
+        int threadGroupsY = Mathf.CeilToInt(Screen.height / 8.0f);
+        RaymarchingShader.Dispatch(0, threadGroupsX, threadGroupsY, 1);
+        // Blit the result texture to the screen
+        Graphics.Blit(_target, destination);
+    }
+    private void InitRenderTexture()
+    {
+        if (_target == null || _target.width != Screen.width || _target.height != Screen.height)
+        {
+            // Release render texture if we already have one
+            if (_target != null)
+                _target.Release();
+            // Get a render target for Ray Tracing
+            _target = new RenderTexture(Screen.width, Screen.height, 0,
+                RenderTextureFormat.ARGBFloat, RenderTextureReadWrite.Linear);
+            _target.enableRandomWrite = true;
+            _target.Create();
+        }
+    }
+
+    // CAMERA
+    private Camera _camera;
+    private void Awake()
+    {
+        _camera = GetComponent<Camera>();
+    }
+    private void SetShaderParameters()
+    {
+        RaymarchingShader.SetMatrix("_CameraToWorld", _camera.cameraToWorldMatrix);
+        RaymarchingShader.SetMatrix("_CameraInverseProjection", _camera.projectionMatrix.inverse);
+    }
+}
